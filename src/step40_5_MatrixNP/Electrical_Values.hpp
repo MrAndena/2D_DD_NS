afc948e553f98362ab7fe99ed2946ff558b72243
Carlo de Falco <carlo.defalco@polimi.it>
2024-05-20 16:39:28 +0000

    clean up

--- src/step40_5_MatrixNP/Electrical_Values.hpp
+++ src/step40_5_MatrixNP/Electrical_Values.hpp
@@ -5,7 +5,8 @@
 #include <deal.II/base/point.h>      // We need the Point class
 #include "Electrical_Constants.hpp"  // header that contains all the constants
 
-// This header file contains the declaration of four template classes that characterize the electrical problem, in particular we have:
+// This header file contains the declaration of four template classes that characterize the electrical problem,
+// in particular we have:
 // DopingValues -- PotentialValues -- ElectronInitialValues -- IonInitialValues
 // These are four template classes, all of them inherit from the template class Function<dim> from deal ii and override the same 
 // method "value". These template classes are used in the methods of the template class Problem.
@@ -13,67 +14,69 @@
 
 // brief description of Function<dim>
 /*
-In the deal.II library, Function<dim> is a template class representing a mathematical function defined on a domain with a specific dimension dim.
-This class provides a framework for defining and evaluating functions that are commonly used in finite element methods.
-It allows users to define functions symbolically or procedurally and provides methods for evaluating these functions at specified points in space.
-The class provides a virtual method: virtual double value(const Point<dim> &p, const unsigned int component = 0) const 
-for evaluating the function at a given point p in space. This method is expected to be overridden to define the specific behavior of the function.
-The optional component parameter allows for computing a specific component of the function's value if it's a vector or tensor function.
-Thus the main idea behinf Function<dim> is to override the method value in order to generate a specific math function for our proposals.
+  In the deal.II library, Function<dim> is a template class representing a mathematical function defined on a
+  domain with a specific dimension dim.
+  This class provides a framework for defining and evaluating functions that are commonly used in finite element methods.
+  It allows users to define functions symbolically or procedurally and provides methods for evaluating these functions at
+  specified points in space.
+  The class provides a virtual method: virtual double value(const Point<dim> &p, const unsigned int component = 0) const 
+  for evaluating the function at a given point p in space. This method is expected to be overridden to define the specific
+  behavior of the function.
+  The optional component parameter allows for computing a specific component of the function's value if it's a vector or
+  tensor function.
+  Thus the main idea behinf Function<dim> is to override the method value in order to generate a specific math function
+  for our proposals.
 */
 using namespace dealii; 
 
-// -------------------------------------- DOPING VALUES ------------------------------------------------------------------------------------------------
-
+// -------------------------------------- DOPING VALUES ------------------------------------------------------------------------
 
 template <int dim>
 class DopingValues : public Function<dim> {
 
-    public:
+public:
      
-   	 DopingValues() : Function<dim>() {}
+  DopingValues() : Function<dim>() {}
 
-     virtual double value(const Point<dim> & p, const unsigned int component = 0) const override; 
+  virtual double value(const Point<dim> & p, const unsigned int component = 0) const override; 
 
 };
 
-// ------------------------------------- POTENTIAL VALUES ---------------------------------------------------------------------------------------------------
+// ------------------------------------- POTENTIAL VALUES ------------------------------------------------------------------------
 
 template <int dim>
 class PotentialValues : public Function<dim> {
 
-    public:
+public:
 
-   	 PotentialValues() : Function<dim>() {}
+  PotentialValues() : Function<dim>() {}
 
-     virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
+  virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
 
 };
 
-// ------------------------------------- ELECTRON INITIAL VALUES ----------------------------------------------------------------------------------------------
-
+// ------------------------------------- ELECTRON INITIAL VALUES ---------------------------------------------------------------
 template <int dim>
 class ElectronInitialValues : public Function<dim> {
 
-    public:
+public:
 
-   	 ElectronInitialValues() : Function<dim>() {}
+  ElectronInitialValues() : Function<dim>() {}
 
-     virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
+  virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
 
 };
 
-// ------------------------------------- ION INITIAL VALUES -------------------------------------------------------------------------------------------
-
+// ------------------------------------- ION INITIAL VALUES --------------------------------------------------------------------
 
 template <int dim>
 class HoleInitialValues : public Function<dim> {
 
-    public:
+public:
 
-   	 HoleInitialValues() : Function<dim>() {}
+  HoleInitialValues() : Function<dim>() {}
 
-     virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
+  virtual double value(const Point<dim> & p, const unsigned int component = 0) const override;
 
 };
 
--- src/step40_5_MatrixNP/Electrical_Values_impl.hpp
+++ src/step40_5_MatrixNP/Electrical_Values_impl.hpp
@@ -1,79 +1,78 @@
 using namespace dealii; 
 
-// ---------------------------------------- DOPING VALUES --------------------------------------------------------------------------------------------------------
-
-// this method return the value of the doping of the pn junction: if we are in the first half (x-axis) of the domain the doping in positive
-// otherwise is negative
+// ---------------------------------------- DOPING VALUES ------------------------------------------------------------------------
+// this method return the value of the doping of the pn junction: if we are in the first half (x-axis) of
+// the domain the doping in positive otherwise is negative
 
 template <int dim>
 double DopingValues<dim>::value(const Point<dim> & p, const unsigned int component) const{
 
-    (void)component; //The function may not use the component parameter, so this line serves to silence any warnings about it being unused.
-    AssertIndexRange(component, 1); //controllare il range di component, probabilmente controlla che la prima coordinata sia dentro il dominio(?)
+  (void)component; //The function may not use the component parameter, so this line serves to silence any warnings about it being unused.
+  AssertIndexRange(component, 1); //controllare il range di component, probabilmente controlla che la prima coordinata sia dentro il dominio(?)
 
-    //assertion that checks whether the template parameter dim equals 2, if dim is not equal to 2, the assertion will fail, 
-    //and an exception of type ExcNotImplemented() will be thrown. This function is only for 2D
-    Assert(dim == 2, ExcNotImplemented()); 
+  //assertion that checks whether the template parameter dim equals 2, if dim is not equal to 2, the assertion will fail, 
+  //and an exception of type ExcNotImplemented() will be thrown. This function is only for 2D
+  Assert(dim == 2, ExcNotImplemented()); 
     
-    // check the x coordinate of the point
-    if (p[0] < 0.5*L)
+  // check the x coordinate of the point
+  if (p[0] < 0.5*L)
     return D;
-    else
+  else
     return -A;
 
 }
 
-// --------------------------------------- POTENTIAL VALUES ---------------------------------------------------------------------------------------------------
+// --------------------------------------- POTENTIAL VALUES --------------------------------------------------------------------
 
 template <int dim>
 double PotentialValues<dim>::value(const Point<dim> & p, const unsigned int component) const{
     
-    // these 3 lines are identical to the first template, same reasoning
-    (void)component;
-    AssertIndexRange(component, 1);
-    Assert(dim == 2, ExcNotImplemented());
+  // these 3 lines are identical to the first template, same reasoning
+  (void)component;
+  AssertIndexRange(component, 1);
+  Assert(dim == 2, ExcNotImplemented());
 
-    if (p[0] <= 0.5*L) // check the x component of the point p
+  if (p[0] <= 0.5*L) // check the x component of the point p
     return V_TH*std::log(D/ni);
-    else
+  else
     return -V_TH*std::log(A/ni);
 
 }
 
-// --------------------------------------- ELECTRON INITIAL VALUES ---------------------------------------------------------------------------------------------
-
-// This method return the value of the electron density, I think we initilaize the first 45% of the domain to N1 density and the other 55% to N2
+// --------------------------------------- ELECTRON INITIAL VALUES -------------------------------------------------------------
+// This method return the value of the electron density, I think we initilaize the first 45% of the domain to
+// N1 density and the other 55% to N2
 
 template <int dim>
 double ElectronInitialValues<dim>::value(const Point<dim> & p, const unsigned int component) const{
     
-    // These 3 lines are identical to the first template, same resoning
-    (void)component;
-    AssertIndexRange(component, 1);
-    Assert(dim == 2, ExcNotImplemented());
+  // These 3 lines are identical to the first template, same resoning
+  (void)component;
+  AssertIndexRange(component, 1);
+  Assert(dim == 2, ExcNotImplemented());
     
-    //check the x component
-    if (p[0] <= 0.45*L)
+  //check the x component
+  if (p[0] <= 0.45*L)
     return N1;
-    else
+  else
     return N2;
 }
 
 
-// --------------------------------------- ION INITIAL VALUES -----------------------------------------------------------------------------------------------------
+// --------------------------------------- ION INITIAL VALUES -------------------------------------------------------------------
 
 
 template <int dim>
 double HoleInitialValues<dim>::value(const Point<dim> & p, const unsigned int component) const {
     
-    // These 3 lines are identical to the first
-    (void)component;
-    AssertIndexRange(component, 1);
-    Assert(dim == 2, ExcNotImplemented());
+  // These 3 lines are identical to the first
+  (void)component;
+  AssertIndexRange(component, 1);
+  Assert(dim == 2, ExcNotImplemented());
     
-    // check the x coordinate of the point
-    if (p[0] <= 0.55*L)
+  // check the x coordinate of the point
+  if (p[0] <= 0.55*L)
     return P1;
-    else
+  else
     return P2;
 }
